2e3756683c27e02b62d8bbf997f917ea9deee9de
Seung <seung@Seungs-MacBook-Pro.local>
2017-10-04 16:55:51 +0900

    tested with a MEMS MIC sensor

--- Processing+Arduino/02-Serial-ADC-basic/sketch_sep27a_ADC_Arduino/sketch_sep27a_ADC_Arduino.ino
+++ Processing+Arduino/02-Serial-ADC-basic/sketch_sep27a_ADC_Arduino/sketch_sep27a_ADC_Arduino.ino
@@ -3,21 +3,27 @@
 // Electret Mic Breakout Board by Sparkfun
 
 int sensorPin = A0;    // select the input pin for the potentiometer
-int ledPin = 13;      // select the pin for the LED
+//int ledPin = 13;      // select the pin for the LED
 int sensorValue = 0;  // variable to store the value coming from the sensor
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(ledPin, OUTPUT);
+//  pinMode(ledPin, OUTPUT);
   
   Serial.begin(9600);
   Serial.println("A0");
 }
 
+// Tested with these two sensors
+// Electret Mic Breakout Board
+// : https://learn.sparkfun.com/tutorials/electret-mic-breakout-board-hookup-guide
+// MEMS Microphone
+// : https://learn.sparkfun.com/tutorials/mems-microphone-hookup-guide
 
-// https://learn.sparkfun.com/tutorials/electret-mic-breakout-board-hookup-guide
 void loop() {
   sensorValue = analogRead(sensorPin);
+  if(sensorValue>1023 || sensorValue<0) 
+  return;
+  
   double volts = (sensorValue * 3.3) / 1024;  // convert to volts
   
   Serial.println(volts);
