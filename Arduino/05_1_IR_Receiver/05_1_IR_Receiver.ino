8a3d72465d4fd39bfcf8dd821d63fccbaab31b93
Seung-Windows <dalek@kaist.ac.kr>
2017-10-25 23:04:46 +0900

    ir receiver

--- Arduino/05_1_IR_Receiver/05_1_IR_Receiver.ino
+++ Arduino/05_1_IR_Receiver/05_1_IR_Receiver.ino
@@ -26,18 +26,26 @@ IRrecv irrecv(RECV_PIN);
 
 #define BTN_H_0 0xC101E57B
 #define BTN_H_1 0x9716BE3F
-//TODO : add other buttons
+#define BTN_H_2 0x3D9AE3F7
+#define BTN_H_3 0x6182021B
+#define BTN_H_4 0x8C22657B
+#define BTN_H_5 0x488F3CBB
+#define BTN_H_6 0x449E79F
+#define BTN_H_7 0x32C6FDF7
+#define BTN_H_8 0x1BC0157B
+#define BTN_H_9 0x3EC3FC1B
+#define BTN_H_100 0x97483BFB
+#define BTN_H_200 0xF0C41643
 
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
   irrecv.enableIRIn();
   Serial.println("Hello");
 }
 
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop() 
+{
   if (irrecv.decode(&results)) 
   {
     Serial.println(results.value, HEX);
@@ -49,10 +57,19 @@ void loop() {
       case BTN_H_0:  
         Serial.println("Button 0"); 
         break;
+
       case BTN1 :
       case BTN_H_1:    
         Serial.println("Button 1"); 
         break;
+
+      case BTN2 :
+      case BTN_H_2:    
+        Serial.println("Button 2"); 
+        break;
+
+      // TODO : Do all the buttons !
+
       default: break;
     
     }
